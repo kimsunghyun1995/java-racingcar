44ddd4c59efc6a595460910eaec4fe9df7273d21
sunghyun <tjdgus2319@gmail.com>
2024-03-16 17:46:36 +0900

    feat: 우승자 뽑는 기능 구현

---
+++ src/main/java/racingCar/Winner.java
@@ -0,0 +1,24 @@
+package racingCar;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Winner {
+
+    private  List<Car> cars;
+
+    public Winner(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    private  int maxPosition() {
+        return cars.stream().map(car ->
+            car.position.getPosition()
+        ).max(Integer::compareTo).orElseThrow(() -> new IllegalArgumentException("position값이 올바르지않습니다."));
+    }
+    public  List<Car> findWinner() {
+        int maxPosition = maxPosition();
+        return cars.stream().filter(car -> car.position.getPosition() == maxPosition
+        ).collect(Collectors.toList());
+    }
+}
